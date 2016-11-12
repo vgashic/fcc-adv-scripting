b477d5df0b8ba043deba6e371cef9c9a230e71a4
Vladimir Gasic <vgashic@gmail.com>
2016-11-12 01:02:42 +0100

    Finished

--- 030 exact change.js
+++ 030 exact change.js
@@ -29,35 +29,21 @@ function checkCashRegister(price, cash, cid) {
 		return [x[0], Math.round(x[1] * 100)];
 	});
 
-	//console.log(cid100);
-
-	// Because of some rounding errors (e.g. 2.05 is like 2.04999999)
-	var cashInRegister = cid.map(function (x) {
-		return Math.round(x[1] * 100);
-	});
+	;
 
 	// Total number of bills in register
 	// Used to avoid infinite loop if change can't be returned'
 	var billsInRegister = cid100.reduce(function (prev, curr) {
-		//console.log(prev + "   " + curr[1]);
 		return prev + curr[1] / coinVal[curr[0]];
 	}, 0);
-	//console.log(billsInRegister);
 
-	// todo: check how to properly reduce 2d array
-	var totalCashInRegister = cashInRegister.reduce(function (prev, curr) {
-		return prev + curr;
+	var totalCashInRegister = cid.reduce(function (prev, curr) {
+		return prev + Math.round(curr[1] * 100);
 	}, 0);
 
-	// debug
-	//  console.log(cashInRegister);
-	//  console.log(totalCashInRegister);
 
 	var changeAmount = Math.round(cash * 100) - Math.round(price * 100);
 
-	// debug
-	//console.log(changeAmount);
-	// console.log(cid);
 
 	if (changeAmount > totalCashInRegister) {
 		return "Insufficient Funds";
@@ -65,7 +51,6 @@ function checkCashRegister(price, cash, cid) {
 		return "Closed";
 	} else {
 		var changeRemains = changeAmount;
-		// console.log(changeRemains > 0);
 
 		// main logic
 		var l = 0;
@@ -73,7 +58,6 @@ function checkCashRegister(price, cash, cid) {
 			l++;
 
 			for (var i = cid100.length - 1; i >= 0; i--) {
-				//console.log(coinVal[cid[i][0]]);
 				if (changeRemains >= coinVal[cid100[i][0]] && cid100[i][1] > 0) {
 					changeRemains -= coinVal[cid100[i][0]];
 
@@ -82,7 +66,6 @@ function checkCashRegister(price, cash, cid) {
 					var found = false;
 
 					for (var j = 0; j < change.length; j++) {
-						//console.log(change[j][0] + "==" + cid100[i][0]);
 						if (change[j][0] == cid100[i][0]) {
 							change[j][1] += coinVal[change[j][0]] / 100;
 							found = true;
@@ -90,29 +73,22 @@ function checkCashRegister(price, cash, cid) {
 						}
 					}
 					if (!found) {
-						//console.log(cid100[i][0] +", " +  coinVal[cid100[i][0]]);
 						change.push([cid100[i][0], coinVal[cid100[i][0]] / 100]);
 					}
 
 
-					//console.log(changeRemains);
 					break;
 				} else {
 					continue;
 				}
 			}
 		}
-		//console.log(billsInRegister);
 		if (billsInRegister < l) {
 			change = [];
 			return "Insufficient Funds";
 		}
 	}
 
-	// Here is your change, ma'am.
-	//console.log(change);
-	//console.log(cid);
-	//console.log(change);
 	return change;
 }
 
@@ -127,8 +103,8 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60.00],
 // ["ONE HUNDRED", 100.00]]
 
-/*
-checkCashRegister(19.50, 20.00, [
+
+console.log(checkCashRegister(19.50, 20.00, [
 	["PENNY", 1.01],
 	["NICKEL", 2.05],
 	["DIME", 3.10],
@@ -138,9 +114,9 @@ checkCashRegister(19.50, 20.00, [
 	["TEN", 20.00],
 	["TWENTY", 60.00],
 	["ONE HUNDRED", 100.00]
-]);
-*/
+]));
 
+/*
 console.log(checkCashRegister(19.50, 20.00, [
 	["PENNY", 0.01],
 	["NICKEL", 0],
@@ -152,3 +128,4 @@ console.log(checkCashRegister(19.50, 20.00, [
 	["TWENTY", 0],
 	["ONE HUNDRED", 0]
 ]));
+*/
